0a9005ed3efd25cd284addbb97cb8b4e9737b58d
Anagas <115894733+anagas-s@users.noreply.github.com>
2024-04-20 19:31:36 +0530

    Setup firebase to the project

---
+++ src/utils/firebase.js
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCxducwun6g0id7UzOsVdnpxDRHlqS7OE8",
+  authDomain: "gptflix-fe7ad.firebaseapp.com",
+  projectId: "gptflix-fe7ad",
+  storageBucket: "gptflix-fe7ad.appspot.com",
+  messagingSenderId: "192678646217",
+  appId: "1:192678646217:web:9034c63af9f4b610dd9818",
+  measurementId: "G-CBJ456BJEC"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
