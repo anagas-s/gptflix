a93ccfc4704de4b3d27875f59760a680679be62e
Anagas <115894733+anagas-s@users.noreply.github.com>
2024-04-20 19:03:25 +0530

    Implement form validation

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,8 +1,22 @@
-import { useState } from "react";
+import { useState,useRef } from "react";
 import Header from "./Header";
+import { checkValidData } from "../utils/validate";
 
 const Login = () => {
     const [isSignInForm,setIsSignInForm] = useState(true);
+    const [errorMessage,setErrorMessage] = useState(null);
+
+
+    const email = useRef(null);
+    const password = useRef(null);
+
+    const handleButtonClick = () => {
+        //Validate the form data
+         
+        const message =  checkValidData(email.current.value,password.current.value);
+        setErrorMessage(message);
+
+    }
 
     const toggleSignInForm = () => {
         setIsSignInForm(!isSignInForm);
@@ -14,12 +28,13 @@ const Login = () => {
         <div>
             <img className="absolute" alt="netflix-bg" src="https://assets.nflxext.com/ffe/siteui/vlv3/fc164b4b-f085-44ee-bb7f-ec7df8539eff/d23a1608-7d90-4da1-93d6-bae2fe60a69b/IN-en-20230814-popsignuptwoweeks-perspective_alpha_website_large.jpg"/>
         </div>
-        <form className="absolute bg-opacity-80 p-12 text-white bg-black w-3/12 my-36 mx-auto right-0 left-0 rounded-lg">
+        <form onSubmit={(e)=>e.preventDefault()} className="absolute bg-opacity-80 p-12 text-white bg-black w-3/12 my-36 mx-auto right-0 left-0 rounded-lg">
             <h1 className="font-bold text-3xl py-4 ">{isSignInForm ? "Sign In" : "Sign Up"}</h1>
            {!isSignInForm && <input type="text" placeholder="Name" className="p-2 my-4 w-full bg-gray-600" />}
-            <input type="text" placeholder="Email Address" className="p-2 my-4 w-full bg-gray-600" />
-            <input type="password" placeholder="Password" className="p-2 my-4 w-full bg-gray-600" />
-            <button className="p-4 my-6 bg-red-600 rounded-lg">{isSignInForm ? "Sign In" : "Sign Up"}</button>
+            <input ref={email} type="text" placeholder="Email Address" className="p-2 my-4 w-full bg-gray-600" />
+            <input ref={password} type="password" placeholder="Password" className="p-2 my-4 w-full bg-gray-600" />
+            <p className="text-red-500 font-bold text-lg py-2">{errorMessage}</p>
+            <button onClick={handleButtonClick} className="p-4 my-6 bg-red-600 rounded-lg">{isSignInForm ? "Sign In" : "Sign Up"}</button>
             <p className="py-4 cursor-pointer font-bold" onClick={toggleSignInForm} >{isSignInForm?"New to Netflix? Sign Up Now":"Already Registered? Sign In Now"}</p>
         </form>
         </div> 
