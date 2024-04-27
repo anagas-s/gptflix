61c488dfaae6e92f4c59f9672b7e4680055740a9
Anagas <115894733+anagas-s@users.noreply.github.com>
2024-04-27 19:43:38 +0530

    Update the user profile and signup functionality

--- src/components/Login.js
+++ src/components/Login.js
@@ -4,13 +4,19 @@ import { checkValidData } from "../utils/validate";
 import { createUserWithEmailAndPassword } from "firebase/auth";
 import { signInWithEmailAndPassword } from "firebase/auth";
 import { auth } from "../utils/firebase";
+import { updateProfile } from "firebase/auth";
 import { useNavigate } from "react-router-dom";
+import { useDispatch } from "react-redux";
+import { addUser } from "../utils/userSlice";
 
 const Login = () => {
     const [isSignInForm,setIsSignInForm] = useState(true);
     const [errorMessage,setErrorMessage] = useState(null);
 
+    const dispatch = useDispatch();
     const navigate = useNavigate();
+
+    const name = useRef(null);
     const email = useRef(null);
     const password = useRef(null);
 
@@ -24,12 +30,27 @@ const Login = () => {
         //Sign In/ Sign Up Logic
         if(!isSignInForm){
             //Sign Up Logic
-            createUserWithEmailAndPassword(auth, email.current.value, password.current.value)
+   createUserWithEmailAndPassword(auth, email.current.value, password.current.value)
   .then((userCredential) => {
     // Signed up 
     const user = userCredential.user;
+    updateProfile(user, {
+      
+        displayName: name.current.value, 
+        photoURL: "https://avatars.githubusercontent.com/u/115894733?v=4",
+      }).then(() => {
+        // Profile updated!
+        const {uid,email,displayName,photoURL}= auth.currentUser;
+              dispatch(addUser({uid:uid,email:email,displayName:displayName,photoURL:photoURL}))
+              navigate("/browse");
+        // ...
+      }).catch((error) => {
+        setErrorMessage(error.message)
+        // An error occurred
+        // ...
+      });
     console.log(user);
-    navigate("/browse")
+   
     // ...
   })
   .catch((error) => {
@@ -68,11 +89,11 @@ const Login = () => {
         </div>
         <form onSubmit={(e)=>e.preventDefault()} className="absolute bg-opacity-80 p-12 text-white bg-black w-3/12 my-36 mx-auto right-0 left-0 rounded-lg">
             <h1 className="font-bold text-3xl py-4 ">{isSignInForm ? "Sign In" : "Sign Up"}</h1>
-           {!isSignInForm && <input type="text" placeholder="Name" className="p-3 my-4 w-full bg-gray-600" />}
+           {!isSignInForm && <input ref={name} type="text" placeholder="Name" className="p-3 my-4 w-full bg-gray-600" />}
             <input ref={email} type="text" placeholder="Email Address" className="p-3 my-4 w-full bg-gray-600" />
             <input ref={password} type="password" placeholder="Password" className="p-3 my-4 w-full bg-gray-600" />
             <p className="text-red-500 font-bold text-lg py-2">{errorMessage}</p>
-            <button onClick={handleButtonClick} className="p-4 my-6 bg-red-600 rounded-lg">{isSignInForm ? "Sign In" : "Sign Up"}</button>
+            <button onClick={handleButtonClick} className="p-4 my-6 bg-red-600 w-full rounded-lg">{isSignInForm ? "Sign In" : "Sign Up"}</button>
             <p className="py-4 cursor-pointer font-bold" onClick={toggleSignInForm} >{isSignInForm?"New to Netflix? Sign Up Now":"Already Registered? Sign In Now"}</p>
         </form>
         </div> 
