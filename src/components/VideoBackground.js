f3888b8f871f69d63c052e76af9686c55b03e1ca
Anagas <anagassatheeshkumar@gmail.com>
2024-07-14 09:56:30 +0530

    Implement autoplay and mute the movie trailer

--- src/components/VideoBackground.js
+++ src/components/VideoBackground.js
@@ -6,19 +6,18 @@ const VideoBackground = ({ movieId }) => {
   const trailerVideo = useSelector((store) => store.movies?.trailerVideo);
   useMovieTrailer(movieId);
   return (
-    <div>
+    <div className="w-screen ">
       <iframe
-        width="560"
-        height="315"
+        className="w-screen aspect-video "
         src={
           "https://www.youtube.com/embed/" +
           trailerVideo?.key +
-          "?autoplay=1&mute=1"
+          "?controls=0?&autoplay=1&mute=1"
         }
         title="YouTube video player"
-        frameBorder="0"
         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
         referrerPolicy="strict-origin-when-cross-origin"
+        allowFullScreen
       ></iframe>
     </div>
   );
--- src/components/VideoTitle.js
+++ src/components/VideoTitle.js
@@ -2,15 +2,16 @@ import React from "react";
 
 const VideoTitle = ({ title, overview }) => {
   return (
-    <div className="pt-36 px-12">
+    <div className="w-screen aspect-video pt-[20%] px-24 absolute text-white bg-gradient-to-r from-black">
       <h1 className="text-6xl font-bold">{title}</h1>
       <p className="py-6 text-sm w-1/4">{overview}</p>
       <div className="flex gap-2">
-        <button className="bg-gray-400 bg-opacity-50 text-black p-3 px-8 rounded-lg text-lg flex ">
+        <button className="bg-white hover:bg-opacity-50 text-black p-3 px-6 rounded-lg text-lg flex gap-2">
           <img src="assets/icons/play.png" width={26} />
           Play
         </button>
-        <button className="bg-gray-400 bg-opacity-50 text-black p-3 px-8 rounded-lg text-lg">
+        <button className="bg-gray-400 bg-opacity-50  text-black p-3 px-4 rounded-lg text-lg flex gap-2">
+          <img src="assets/icons/information.png" width={26} />
           More Info
         </button>
       </div>
