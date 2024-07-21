0e4e2667a59ac216a1c9665b0be7afdc0f2768f3
Anagas <anagassatheeshkumar@gmail.com>
2024-07-21 10:43:42 +0530

    update the api

--- src/hooks/useNowPlayingMovies.js
+++ src/hooks/useNowPlayingMovies.js
@@ -11,12 +11,12 @@ const useNowPlayingMovies = () => {
   );
   const getNowPlayingMovies = async () => {
     const data = await fetch(
-      "https://thingproxy.freeboard.io/fetch/https://api.themoviedb.org/3/movie/now_playing?language=en-US&page=1",
+      "https://express-vercel-xi-gules.vercel.app/proxy",
       API_OPTIONS
     );
 
     const json = await data.json();
-
+    console.log(json.results);
     dispatch(addNowPlayingMovies(json.results));
   };
   useEffect(() => {
